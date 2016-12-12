8efce20eca9e3742a0c049710ede44361e342a9b
Jorge Luis Canchón Espinosa <jlcanchon@misena.edu.co>
2016-12-12 10:28:01 -0500

    Update cerrarSesion.php

--- cerrarSesion.php
+++ cerrarSesion.php
@@ -1,7 +1,7 @@
 <?php 
 session_start();
-unset($SESSION['estado']);
-unset($SESSION['usuario']);
-$SESSION['estado']=0;
+unset($_SESSION['estado']);
+unset($_SESSION['usuario']);
+$_SESSION['estado']=0;
 header("Location:../publicidad/loguin.php")
  ?>
