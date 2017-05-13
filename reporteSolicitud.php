06fd468f9d32e62939cbcf6dbccb7fc7b32714cf
Jorge Luis Canchón Espinosa <jlcanchon@misena.edu.co>
2017-05-12 20:20:32 -0500

    Add files via upload

---
+++ reporteSolicitud.php
@@ -0,0 +1,30 @@
+<?php 
+session_start();
+if ($_SESSION['estado']==0) {
+  header("Location:../publicidad/loguin.php");
+}
+ ?>
+<?php
+error_reporting(0); 
+include_once 'conectar.php';
+header('Content-type: application/vnd.ms-word');
+header("Content-Disposition: attachment; filename=SOLICITUDMATERIAL.doc");
+ header("Pragma: no-cache");
+header("Expires: 0");
+?>
+<style type="text/css">
+ 		 .tablalistado{
+      background-color: white;
+      border-collapse: collapse;
+      box-shadow: 0px 0px 8px #000;
+      margin: auto;
+    }
+    .tablalistado th{
+      border:1px solid #6A9E98;
+    }
+    .tablalistado td{
+      border: 1px solid #000;
+      padding: 5px;
+      background-color: #ffdd73; 
+    }
+ </style>	
